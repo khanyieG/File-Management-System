a65ee832964287981c86431c6207ba9341830aac
kgama021 <kgama021@student.wethinkcode.co.za>
2024-02-14 22:04:14 +0200

    fixed gaming function

--- gaming.py
+++ gaming.py
@@ -3,7 +3,7 @@ import os
 def gaming(path):
     gaming_path = path / "Gaming"  # Use pathlib for concise path joining
     gaming_path.mkdir(parents=True, exist_ok=True)
-    code_path= gaming / "Code"
+    code_path= gaming_path / "Code"
     code_path.mkdir(parents=True, exist_ok=True)
     animation_path= gaming_path / "Animation"
     animation_path.mkdir(parents=True, exist_ok=True)  
